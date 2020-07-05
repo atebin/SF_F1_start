fb501405c4d644c01a6ad63704f4217810131258
a_tebin <a.tebin@gmail.com>
2020-07-05 17:04:57 +0300

    SF_F1_start: completed work on version 1

--- script.js
+++ script.js
@@ -1,70 +1,88 @@
 let objSvetofor = {
     linksToReds: {
-        item1: null,
-        item2: null,
-        item3: null,
-        item4: null,
-        item5: null,
+        item1: document.querySelectorAll('.avt-svetofor-item1 .avt-svetofor-item-lamp--red'),
+        item2: document.querySelectorAll('.avt-svetofor-item2 .avt-svetofor-item-lamp--red'),
+        item3: document.querySelectorAll('.avt-svetofor-item3 .avt-svetofor-item-lamp--red'),
+        item4: document.querySelectorAll('.avt-svetofor-item4 .avt-svetofor-item-lamp--red'),
+        item5: document.querySelectorAll('.avt-svetofor-item5 .avt-svetofor-item-lamp--red'),
     },
     linksToGreens: document.querySelectorAll('.avt-svetofor-item-lamp--off'),
-    linkToResult: document.querySelector('.avt-result')
+    linkToResultTime: document.querySelector('.avt-result-time'),
+    linkToResultStatus: document.querySelector('.avt-result-status'),
+    timerIdRed: null,
+    timerIdGreen: null,
+    currentStatus: '-',
+    countImpuls: 0,
 }
-
-let timerIdInterval = null;
-let currStepCount = 0;
-let currTime = '-';
 
 InitPage();
 
 function InitPage(){
-    let myBody = document.body;
-    myBody.addEventListener('click', bodyClick);
+    document.body.addEventListener('click', bodyClickPrepair);
 
-    let allReds = objSvetofor.linksToReds;
-    for (elem in allReds){
-        allReds[elem] = document.querySelectorAll('.avt-svetofor-' + elem +' .avt-svetofor-item-lamp--red');
-    }
-    console.log(objSvetofor);
 }
 
-function bodyClick(event) {
-    if (currStepCount == 0) {
-        currTime = 'фальшстарт!!!'; 
-        timerIdInterval = setInterval(StepCount, 800); 
-    } else if (currTime == 'фальшстарт!!!') {
-        objSvetofor.linkToResult.innerHTML = currTime;
-        clearInterval(timerIdInterval);
-    } else {
-        objSvetofor.linkToResult.innerHTML = currTime;
-    }
+function bodyClickPrepair(event) {
+    let currStepCount = 0;
+    document.body.removeEventListener('click', bodyClickPrepair);
+    document.body.addEventListener('click', bodyClickFail);
+    document.body.style.backgroundColor = 'gray';
 
+    objSvetofor.timerIdRed = setInterval(() => {
+        // меняем обработчики на click
+        if (++currStepCount <= 5) {
+            console.log(currStepCount);
+            // гасим красные огни, пока они есть на табло
+            let currReds = [...objSvetofor.linksToReds['item' + currStepCount]];
+            currReds.forEach(elem => {
+                elem.classList.toggle('avt-svetofor-item-lamp--red');
+                elem.classList.toggle('avt-svetofor-item-lamp--off');
+            });
+        } else {
+            // останавливаем таймер с отсчетом времени
+            clearInterval(objSvetofor.timerIdRed);
+
+            // меняем обработчики на click
+            document.body.removeEventListener('click', bodyClickFail);
+            document.body.addEventListener('click', bodyClickSuccess);
+
+            // зажигаем зеленые огни
+            let currGreens = [...objSvetofor.linksToGreens];
+            currGreens.forEach(elem => {
+                elem.classList.toggle('avt-svetofor-item-lamp--off');
+                elem.classList.toggle('avt-svetofor-item-lamp--green');
+            });
+
+            //
+           objSvetofor.timerIdRed = setInterval(() => {
+                ++objSvetofor.countImpuls;
+            },5);
+        }
+    }, 800); 
+}
+
+function ImpulseParser() {
     
+    clearInterval(objSvetofor.timerIdGreen);
+    let intervalInMilliSec = 5;
+    let result = objSvetofor.countImpuls * intervalInMilliSec / 1000;
+    
+    return result.toFixed(3);
 }
 
-function StepCount(){
-    if (++currStepCount <= 5) {
-        let currReds = [...objSvetofor.linksToReds['item' + currStepCount]];
-        
-        currReds.forEach(elem => {
-            elem.classList.toggle('avt-svetofor-item-lamp--red');
-            elem.classList.toggle('avt-svetofor-item-lamp--off');
-        });
-    } else {
-        clearInterval(timerIdInterval);
-        //LetSGo();
-
-        let currGreens = [...objSvetofor.linksToGreens];
-        
-        currGreens.forEach(elem => {
-            elem.classList.toggle('avt-svetofor-item-lamp--off');
-            elem.classList.toggle('avt-svetofor-item-lamp--green');
-        });
-        currTime = "Success!";
-        //currStepCount = -1;
-
-    }
+function bodyClickFail(){
+    document.body.removeEventListener('click', bodyClickFail);
+    clearInterval(objSvetofor.timerIdRed);
+    objSvetofor.linkToResultTime.innerHTML = '0.000';
+    objSvetofor.linkToResultStatus.innerHTML = 'фальшстарт!';
+    objSvetofor.linkToResultStatus.style.color = '#a00';
+    document.body.style.backgroundColor = 'lightgrey';
 }
 
-function LetSGo() {
-    console.log('GO !!!');
+function bodyClickSuccess() {
+    document.body.removeEventListener('click', bodyClickSuccess);
+    objSvetofor.linkToResultTime.innerHTML = ImpulseParser();
+    objSvetofor.linkToResultStatus.innerHTML = 'Success!';
+    objSvetofor.linkToResultStatus.style.color = '#3b3';
+    document.body.style.backgroundColor = 'lightgrey';
 }
